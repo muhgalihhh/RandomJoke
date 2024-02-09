9adf6dfbe66f98a72c9d9ec7c2e0c7fba41b76db
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-02-09 07:34:15 +0700

    while finished

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const API_URL = 'https://v2.jokeapi.dev/joke/Any';
+const API_URL = 'https://v2.jokeapi.dev/joke/';
 
 // get
 app.get('/', (req, res) => {
   res.render('index.ejs', { type:"API Response" });
 });
 
+//post
+
 app.post('/randomJoke', async (req, res) => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(API_URL+'Any');
     const result = response.data;
     res.render('index.ejs', { 
       joke: result.joke, 
@@ -30,6 +32,23 @@ app.post('/randomJoke', async (req, res) => {
   }
 }
 );
+
+app.post('/search', async (req, res) => {
+  try{
+    const category = req.body.category;
+    const type = req.body.type;
+    const response = await axios.get(`${API_URL}${category}?type=${type}`);
+    const result = response.data;
+    res.render('index.ejs', { 
+      joke: result.joke, 
+      category: result.category, 
+      type: result.type, 
+      setup: result.setup, 
+      delivery: result.delivery });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 
 
